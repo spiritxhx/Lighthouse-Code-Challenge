2ead9031bd70e5642be2ef6fc2ea3e5029e904ad
spiritxhx <spiritxhx@gmail.com>
2019-11-09 19:07:42 -0500

    day 8 and day 9

---
+++ 8.js
@@ -0,0 +1,5 @@
+const smartGarbage = (trash, bins) => {
+  // Code here!
+  bins[trash]++;
+  return bins;
+};
---
+++ 9.js
@@ -0,0 +1,5 @@
+const carPassing = (cars, speed) => {
+  // Code here!
+  cars.push({ time: Date.now(), speed: [speed] });
+  return cars;
+};
