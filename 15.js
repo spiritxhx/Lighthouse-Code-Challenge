b0b36830772b9e8692174c6154b33b06f73fee86
spiritxhx <spiritxhx@gmail.com>
2019-11-15 23:42:31 -0500

    challenge 15

---
+++ 15.js
@@ -0,0 +1,12 @@
+const finalPosition = (moves) => {
+  const ans=[0,0];
+  moves.map(move=>{
+    switch(move){
+      case 'north':ans[1]++;break;
+      case 'south':ans[1]--;break;
+      case 'east': ans[0]++;break;
+      case 'west': ans[0]--;break;
+    }
+  });
+  return ans;
+}
